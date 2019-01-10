775d135cc2fe3278f2e0c40e40e9d59e3527878e
s4017050400 <s4017050400@gmail.com>
2019-01-10 23:52:40 +0800

    Create open_path.py

---
+++ open_path.py
@@ -0,0 +1,7 @@
+path=r"C:\Users\s4017\Desktop\test.txt" #prefix the string with r (to produce a raw string) 
+fout=open(path,'w')
+str1='hello world'
+fout.write(str1)
+fout.close()
+
+#write txt to spectific path
